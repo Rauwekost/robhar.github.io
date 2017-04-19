49825aabf26090e288db9ca7a0cfb2431c358715
Robert den Harink <robert@robhar.com>
2017-04-19 10:49:35 +0200

    Publish.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Stash changes.
+git stash
+
+# Checkout develop and build
+git checkout develop
+stack exec site clean
+stack exec site build
+
+# Checkout master and copy changes
+git fetch --all
+git checkout master
+cp -a _site/. .
+
+# Commit and push
+if ! git diff-index --quiet HEAD --; then
+    git add -A
+    git commit -m "Publish."
+    git push origin master:master
+else
+    echo "No changes..."
+fi
+
+
+# Reset back to develop.
+git checkout develop
+git branch -D master
+git stash pop
